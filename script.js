8b8755198062ba731b90c2d6536cd33f7f11605c
Serhii <stupanenko.serhii@gmail.com>
2023-08-12 15:52:42 +0300

    implement the grid resizing logic

--- script.js
+++ script.js
@@ -2,20 +2,41 @@ let size = 16;
 // let buttons = document.querySelectorAll('.buttons button');
 window.addEventListener('load', ()=> {
     createGrid();
-    document.querySelector('#size').addEventListener('change', (e)=>{
-        size = e.target.value;
-        createGrid();
-    })
-
+    
     const pencil_btn = document.querySelector('.pencil');
     const eraser_btn = document.querySelector('.eraser');
     const clear_btn = document.querySelector('.clear_btn');
+    // const zoomIn_btn = document.querySelector('.zoomIn_btn');
+    // const zoomOut_btn = document.querySelector('.zoomOut_btn');
     // const random_btn = document.querySelector('.random');
     pencil_btn.addEventListener('click', ()=>changeMode('pencil'))
     eraser_btn.addEventListener('click', ()=>changeMode('eraser'))
     // random_btn.addEventListener('click', ()=>changeMode('random'))
-    clear_btn.addEventListener('click', createGrid)
+    clear_btn.addEventListener('click', createGrid);
+    // zoomIn_btn.addEventListener('click', zoomIn);
+    // zoomOut_btn.addEventListener('click', zoomOut);
+    document.querySelector('input[type="range"]').addEventListener('input', (e)=> {
+        document.querySelector('label[for="sizeR"]').textContent = `${e.target.value}x${e.target.value}`
+        // setSize(e);
+    })
+    document.querySelector('input[type="range"]').addEventListener('change', (e)=>setSize(e))
+
 })
+function setSize(e){
+    size = e.target.value;
+    document.querySelector('input[type="range"]').value = e.target.value;
+    createGrid();
+}
+// function zoomIn(){
+//     let grid = document.querySelector('.sketchpad');
+//     grid.style.width = `${grid.clientWidth + 100}px`;
+//     grid.style.height = `${grid.clientHeight + 100}px`;
+// }
+// function zoomOut(){
+//     let grid = document.querySelector('.sketchpad');
+//     grid.style.width = `${grid.clientWidth - 100}px`;
+//     grid.style.height = `${grid.clientHeight - 100}px`;
+// }
 
 
 let current_mode = 'pencil';
